7af8de62beef82adb0f7f45d9f0d55143b1135f5
geleigh <57293957+geleigh@users.noreply.github.com>
2020-04-19 22:29:32 -0400

    establishing map
    establishing map

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,11 @@
+var myMap = L.map("map", {
+    center: [34.0522, -118.2437],
+    zoom: 8
+  });
+
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.cali-terrain",
+    accessToken: API_KEY
+    });
